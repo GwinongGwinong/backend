728e0264086e13b6b4dbc8652f817adb5b8f5ce3
Inderby <youk8253@gmail.com>
2023-09-10 22:50:49 +0900

    #8 - test db 관련 설정들

--- src/test/java/com/example/farmingholiday/FarmingHolidayApplicationTests.java
+++ src/test/java/com/example/farmingholiday/FarmingHolidayApplicationTests.java
@@ -1,8 +1,16 @@
 package com.example.farmingholiday;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@EnableJdbcRepositories
+@ComponentScan(basePackages = "src/main/java/com/example/farmingholiday/repository")
+@EntityScan(basePackages = "src/main/java/com/example/farmingholiday/domain")
 @SpringBootTest
 class FarmingHolidayApplicationTests {
 
